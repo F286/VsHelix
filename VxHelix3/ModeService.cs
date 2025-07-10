6e05afc74e09ac74a9142262eb28884bc63ccdee
F286 <freedebreuil@gmail.com>
2025-07-10 00:01:21 -0500

    Basic version working (rewrite) with VSX approach and legacy compat of ICommandHandler

---
+++ VxHelix3/ModeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace VxHelix3
+{
+	[VisualStudioContribution]               // pick any namespace
+	public sealed class ModeService
+	{
+		public enum EditorMode { Normal, Insert }
+		public EditorMode Current { get; private set; } = EditorMode.Normal;
+
+		public void EnterInsert() => Current = EditorMode.Insert;
+		public void EnterNormal() => Current = EditorMode.Normal;
+	}
+
+}
---
+++ VxHelix3/TypeCharFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VxHelix3
+{
+	[Export(typeof(ICommandHandler))]
+	[ContentType("text")]
+	[TextViewRole(PredefinedTextViewRoles.Editable)]
+	[Name(nameof(TypeCharFilter))]
+	[Order(Before = "TypeChar")]
+	[VisualStudioContribution]
+	internal sealed class TypeCharFilter
+			   : ICommandHandler<TypeCharCommandArgs>
+	{
+		public string DisplayName => "Helix Emulation (demo)";
+
+		public CommandState GetCommandState(TypeCharCommandArgs args)
+			=> CommandState.Unspecified;        // let VS decide enable/disable
+
+		public bool ExecuteCommand(TypeCharCommandArgs args,
+								   CommandExecutionContext ctx)
+		{
+			// Example: on 'w' expand to "word"
+			if (args.TypedChar == 'w')
+			{
+				var view = args.TextView;
+				using var edit = view.TextBuffer.CreateEdit();
+				edit.Insert(view.Caret.Position.BufferPosition, "word");
+				edit.Apply();
+				return true;                   // we handled it, stop bubbling
+			}
+
+			return false;                      // not ours, pass to next handler
+		}
+	}
+}
